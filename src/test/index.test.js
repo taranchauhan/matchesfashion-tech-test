9666d888592f53cd82e2dfbeb51b95fdb4e086dc
Taran Chauhan <taran.chauhan@hotmail.com>
2020-01-17 15:17:56 +0000

    TDD for expected array of objects

--- src/test/index.test.js
+++ src/test/index.test.js
@@ -1,18 +1,48 @@
 const assert = require('assert');
 
-const { fetchURLList } = require('../');
+const { fetchUrlList } = require('../');
 
 describe('Fetch URL list', function() {
-  it('it should fetch an array of objects', function() {
+  it('it should fetch an array of objects', async function() {
     const expected = [
       {
-        name: 'product name',
-        url: '/test/123',
-        price: 10,
-        thumbnail: 'https://thumb_url/123.jpg'
+        name: 'V-neck wool sweater',
+        url: '/products/Adam-Lippes-V-neck-wool-sweater-1095470',
+        price: 121,
+        thumbnail:
+          '//assetsprx.matchesfashion.com/img/product/1095470_1_thumbnail.jpg'
+      },
+      {
+        name: 'Straight-leg chino trousers ',
+        url: '/products/Adam-Lippes-Straight-leg-chino-trousers--1095483',
+        price: 117,
+        thumbnail:
+          '//assetsprx.matchesfashion.com/img/product/1095483_1_thumbnail.jpg'
+      },
+      {
+        name: 'Crew-neck wool sweater ',
+        url: '/products/Adam-Lippes-Crew-neck-wool-sweater--1095472',
+        price: 285,
+        thumbnail:
+          '//assetsprx.matchesfashion.com/img/product/1095472_1_thumbnail.jpg'
+      },
+      {
+        name: 'Crew-neck wool sweater',
+        url: '/products/Adam-Lippes-Crew-neck-wool-sweater-1095467',
+        price: 202,
+        thumbnail:
+          '//assetsprx.matchesfashion.com/img/product/1095467_1_thumbnail.jpg'
+      },
+      {
+        name: 'V-neck wool sweater',
+        url: undefined,
+        price: undefined,
+        thumbnail:
+          '//assetsprx.matchesfashion.com/img/product/1095470_1_thumbnail.jpg'
       }
     ];
 
-    assert.deepEqual(fetchURLList(), expected);
+    const actual = await fetchUrlList();
+    assert.deepEqual(actual, expected);
   });
 });
